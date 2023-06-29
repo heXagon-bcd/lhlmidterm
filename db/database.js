9105b16f9e739c9db547b03ae8d445f8e7481b4e
Jelani Sanders <jelanisands@gmail.com>
2023-06-28 23:59:46 -0400

    Add password to database.js pool variable

--- db/database.js
+++ db/database.js
@@ -2,55 +2,55 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'labber',
-  password: '',
+  password: 'labber',
   host: 'localhost',
   database: 'midterm'
 });
 
 
-const getTasksWithUsers = function (email) {
+const getTasksWithUsers = function(email) {
   const queryString = `
   select *
   from tasks t
   join users u on t.user_id = u.id
   where u.username = $1;
-  `
+  `;
   return pool
     .query(queryString, [email])
-    .then( (result) => {
+    .then((result) => {
       console.log("getTaskswithUsers", result.rows);
       return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
     });
-}
+};
 
-const addTask = function (task_description) {
- const queryString = `
+const addTask = function(task_description) {
+  const queryString = `
  INSERT INTO tasks (name, user_id, category_id) VALUES ($1, 1, 5)
  RETURNING *;
- `
- return pool
- .query(queryString, [task_description])
- .then( (result) => {
-  console.log("add task", result.rows);
-   return result.rows;
- })
- .catch((err) => {
-   console.log(err.message);
- });
-}
+ `;
+  return pool
+    .query(queryString, [task_description])
+    .then((result) => {
+      console.log("add task", result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
-const editTask = function (user) {
+const editTask = function(user) {
 
-}
+};
 
-const changeTask = function (user) {
+const changeTask = function(user) {
 
-}
+};
 
-console.log(getTasksWithUsers('alice@gmail.com'))
+console.log(getTasksWithUsers('alice@gmail.com'));
 
 module.exports = {
   getTasksWithUsers,

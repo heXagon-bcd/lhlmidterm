19dce46dd3938ae26d081508ceac2913c8faf863
Shawn He <shawnhe1991@gmail.com>
2023-06-28 22:42:10 -0400

    complete add task function

--- public/scripts/tasks.js
+++ public/scripts/tasks.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   const createTaskElement = function (task) {
     const $taskContainer = $(`
-  <p>${task.task_description}</p>
+  <li>${task.task_description}</li>
   `);
     $(".card-contents").append($taskContainer);
   };
@@ -18,13 +18,33 @@ $(document).ready(function () {
 
   const loadTasks = function () {
     console.log("loading tasks");
-    $.ajax("/api/taskRoutes", { method: "GET" })
-     .then(function (response) {
-      console.log("success", response);
-      renderTasks(response);
+    $.ajax("/api/taskRoutes", { method: "GET" })// front end is visiting the site in the background, response from db --> tasksRoutes.js(server) --> db query
+     .then(function (tasks) {//pushed to front end as array/object(depending on format from server)
+      console.log("success", tasks);
+      renderTasks(tasks);
     });
   };
-
   //load tasks wtihout needing to press intial submit
   loadTasks();
+
+
+  //add tasks function
+  $("#task-form").on("submit", function(event) {
+    event.preventDefault()// to prevent the default form submission behaviour.
+    const text = $(this).serialize();
+    console.log("input text", text);
+    $.ajax ({
+    type: "POST",
+    url: "/api/taskRoutes",
+    data: text,
+    success: function (){
+      $.ajax("/api/taskRoutes", { method: "GET" })
+      .then(function(input) {
+        console.log("sucesss", input);
+        const latestPost = input[input.length - 1];
+        createTaskElement(latestPost)
+        })
+      }
+    })
+  })
 });
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,38 +4,40 @@ const database = require("../db/database");
 const router = express.Router();
 
 //create tasks
-router.post('/', (req,res) => {
+router.post("/", (req, res) => {
+  // look at database.js  to make sure addTask
+  console.log("req.body.task_description", req.body.task_description);
   database
-    .addTask(req.body)
+    .addTask(req.body.task_description)
     .then((tasks) => {
       res.send(tasks);
     })
     .catch((e) => {
       console.error(e);
+      res.status(500).send(e);//review doc on status -> if no status delcare, front end ajax will determine as success
+    });
+});
+
+//retrieve tasks
+router.get("/", (req, res) => {
+  console.log("req", req.query);
+  database
+    .getTasksWithUsers("alice@gmail.com")
+    .then((tasks) => res.send(tasks))
+    .catch((e) => {
+      console.error(e);
       res.send(e);
     });
-})
-//retrieve tasks
-router.get('/', (req,res) => {
-  console.log("req", req.query)
-  database
-  .getTasksWithUsers('alice@gmail.com')
-  .then((tasks) => res.send(tasks))
-  .catch((e) => {
-    console.error(e);
-    res.send(e);
-  });
-})
+});
 
 //update tasks
-router.put('/', (req,res) => {
-  res.send('Hi shawn');
-})
+router.put("/", (req, res) => {
+  res.send("Hi shawn");
+});
 
 //delete tasks
-router.delete('/', (req,res) => {
-  res.send('Hi shawn');
-})
-
+router.delete("/", (req, res) => {
+  res.send("Hi shawn");
+});
 
 module.exports = router;

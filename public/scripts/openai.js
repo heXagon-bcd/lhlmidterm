7865e2463e3ee8f7e6a8c34c74c0678135a82271
Shawn He <shawnhe1991@gmail.com>
2023-06-30 14:57:21 -0400

    improved ai prompt

--- public/scripts/openai.js
+++ public/scripts/openai.js
@@ -12,13 +12,18 @@ const categorizeInput = (input) => {
 
   return openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `I have 4 categories. 1: Movies, 2: Books, 4: Restaurants, 3: Products, and 5: Uncategorized. Please categorize my following prompt, using only the number associated with the category. For example: I want to eat at nobu should return 4.\n\n${input}\n\nCategory Number: \n\n`,
+    prompt: `I have 4 categories. 1: Movies, 2: Books, 3: Products, 4: Restaurants, and 5: Uncategorized.
+    Please categorize my following prompt, using only the number.
+    Return 5 if the falls into multiple categories. example: I want to eat at nobu should return 4 beceause user wants to eat.
+    Here's an example of a prompt that could have multiple categories: spiderman returns 5 because there are books, movies, and products associated with it.
+    Another example: Watch batman reuturns 1 because the user indicates they want to watch a movie.
+    ONLY return a number.\n\n${input}\n\nCategory Number:`,
     temperature: 0,
-    max_tokens: 6,
+    max_tokens: 1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    stop: ["\n"],
+    // stop: ["\n"],
   })
     .then((response) => {
       return response.data.choices[0].text;
